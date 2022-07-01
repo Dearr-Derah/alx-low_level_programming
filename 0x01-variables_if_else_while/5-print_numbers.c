f13513f6512dbb2a3603f3f6c86d8600c695ec39
Your Name <you@example.com>
2022-07-01 15:44:26 -0700

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
